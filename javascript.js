2e306b67efbb3251b767d48f52ba3d3ec8d69967
yash-wakde <wakdeyash@gmail.com>
2023-09-03 09:13:04 +0530

    Add playRound

--- javascript.js
+++ javascript.js
@@ -6,37 +6,48 @@ function getComputerChoice(){
 
 
 function playRound(){
-    let playerSelection = prompt("Enter Rock, Paper or Scissors : ").toLowerCase();
-    let computerSelection = getComputerChoice();
+    if (playerSelection == "rock" && computerSelection == "paper"){
+    return "You Lose! Paper beats Rock!";
+    }
 
-    if (playerSelection == "rock" && computerSelection == "paper")
-    console.log("You Lose! Paper beats Rock!")
+    else if (playerSelection == "rock" && computerSelection == "scissors"){
+        return  "You Win! Rock beats Scissors";
+    }
 
-    else if (playerSelection == "rock" && computerSelection == "scissors")
-    console.log("You Win! Rock beats Scissors")
+    else if (playerSelection == "rock" && computerSelection == "rock"){
+        return "It's a Tie!";
+    }
 
-    else if (playerSelection == "rock" && computerSelection == "rock")
-    console.log("It's a Tie!")
+    else if (playerSelection == "paper" && computerSelection == "scissors"){
+        return "You Lose! Scissors beat Paper";
+    }
 
-    else if (playerSelection == "paper" && computerSelection == "scissors")
-    console.log("You Lose! Scissors beat Paper")
+    else if (playerSelection == "paper" && computerSelection == "rock"){
+        return "You Win! Paper beats Rock";
+    }
 
-    else if (playerSelection == "paper" && computerSelection == "rock")
-    console.log("You Win! Paper beats Rock")
+    else if (playerSelection == "paper" && computerSelection == "paper"){
+        return "It's a Tie!"
+    }
 
-    else if (playerSelection == "paper" && computerSelection == "paper")
-    console.log("It's a Tie!")
+    else if (playerSelection == "scissors" && computerSelection == "rock"){
+        return "You Lose! Rock beats Scissors";
+    }
 
-    else if (playerSelection == "scissors" && computerSelection == "rock")
-    console.log("You Lose! Rock beats Scissors")
+    else if (playerSelection == "scissors" && computerSelection == "paper"){
+        return "You Win! Scissors beats Paper";
+    }
 
-    else if (playerSelection == "scissors" && computerSelection == "paper")
-    console.log("You Win! Scissors beats Paper")
+    else if (playerSelection == "scissors" && computerSelection == "scissors"){
+        return "It's a Tie!";
+    }
 
-    else if (playerSelection == "scissors" && computerSelection == "scissors")
-    console.log("It's a Tie!")
-
-    else console.log("Invalid input")
+    else {
+        return "Not a Valid Input";
+    }
 }
 
-playRound();
+let playerSelection = prompt("Enter Rock, Paper or Scissors : ").toLowerCase();
+let computerSelection = getComputerChoice();
+
+console.log(playRound(playerSelection, computerSelection));
