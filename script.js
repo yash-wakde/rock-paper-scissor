dabeee8e6ef18968228f052657b8aed55923be10
yash-wakde <wakdeyash@gmail.com>
2023-09-11 03:22:49 +0530

    Add Final Winner

--- script.js
+++ script.js
@@ -1,60 +1,68 @@
-const playerText = document.querySelector('.playerText')
-const computerText = document.querySelector('.computerText')
-const resultText = document.querySelector('.resultText')
-const selectionButtons = document.querySelectorAll('[data-selection]')
-const playerScore = document.querySelector('.player-score')
-const computerScore = document.querySelector('.computer-score')
+const playerText = document.querySelector('.playerText');
+const computerText = document.querySelector('.computerText');
+const resultText = document.querySelector('.resultText');
+const selectionButtons = document.querySelectorAll('[data-selection]');
+const playerScore = document.querySelector('.player-score');
+const computerScore = document.querySelector('.computer-score');
 
-let player
-let computer
-let result
-let playerWin = "You Win!"
-let playerLose = "You Lose!"
-let gameTie = "It's a Tie!"
+let player = 0;
+let computer = 0;
+const maxScore = 5; 
 
-selectionButtons.forEach(selection =>{
-    selection.addEventListener('click', e =>{
-        player = selection.textContent 
-        computer = getComputerChoice()
-        playerText.textContent = `You : ${player}`
-        computerText.textContent = `Computer : ${computer}`
-        resultText.textContent = playRound()
-    })
-})
+let playerWin = "You Win!";
+let playerLose = "You Lose!";
+let gameTie = "It's a Tie!";
 
-function getComputerChoice(){
-    let items = ["✊","✋","✌️"]
-    let answer = items[Math.floor(Math.random()*items.length)]
-    return answer
+selectionButtons.forEach(selection => {
+    selection.addEventListener('click', e => {
+        if (player < maxScore && computer < maxScore) {
+            const playerChoice = selection.textContent;
+            const computerChoice = getComputerChoice();
+            playerText.textContent = `You : ${playerChoice}`;
+            computerText.textContent = `Computer : ${computerChoice}`;
+            const roundResult = playRound(playerChoice, computerChoice);
+            resultText.textContent = roundResult;
+
+            if (player === maxScore || computer === maxScore) {
+                announceWinner();
+            }
+        }
+    });
+});
+
+function getComputerChoice() {
+    const items = ["✊", "✋", "✌️"];
+    const answer = items[Math.floor(Math.random() * items.length)];
+    return answer;
 }
 
-function playRound(){
-    if (player ==  computer){
-        return gameTie}
-
-    else if (player == "✊" && computer == "✋"){
-        computerScore.innerText = parseInt(computerScore.innerText)+1
-        return playerLose}
-    
-    else if (player == "✊" && computer == "✌️"){
-        playerScore.innerText = parseInt(playerScore.innerText)+1
-        return  playerWin}
-    
-    else if (player == "✋" && computer == "✌️"){
-        computerScore.innerText = parseInt(computerScore.innerText)+1
-        return playerLose}
-
-    else if (player == "✋" && computer == "✊"){
-        playerScore.innerText = parseInt(playerScore.innerText)+1
-        return playerWin}
-    
-    else if (player == "✌️" && computer == "✊"){
-        computerScore.innerText = parseInt(computerScore.innerText)+1
-        return playerLose}
-
-    else if (player == "✌️" && computer == "✋"){
-        playerScore.innerText = parseInt(playerScore.innerText)+1
-        return playerWin}
+function playRound(playerChoice, computerChoice) {
+    if (playerChoice === computerChoice) {
+        return gameTie;
+    } else if (
+        (playerChoice === "✊" && computerChoice === "✋") ||
+        (playerChoice === "✋" && computerChoice === "✌️") ||
+        (playerChoice === "✌️" && computerChoice === "✊")
+    ) {
+        computer++;
+        computerScore.innerText = computer;
+        return playerLose;
+    } else {
+        player++;
+        playerScore.innerText = player;
+        return playerWin;
+    }
 }
 
+function announceWinner() {
+    if (player === maxScore) {
+        resultText.textContent = "You are the overall winner!";
+    } else {
+        resultText.textContent = "Computer is the overall winner!";
+    }
 
+    selectionButtons.forEach(selection => {
+        selection.removeEventListener('click', () => {});
+        selection.disabled = true;
+    });
+}
