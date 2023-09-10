888465f560afb02a9aa5053a095dcd7df1e60727
yash-wakde <wakdeyash@gmail.com>
2023-09-11 00:20:27 +0530

    Add RPS Logic using JS

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const playerText = document.querySelector('#playerText')
 const computerText = document.querySelector('#computerText')
 const resultText = document.querySelector('#resultText')
-const selections = document.querySelectorAll('.selections')
+const selectionButtons = document.querySelectorAll('[data-selection]')
 
 let player
 let computer
@@ -10,13 +10,15 @@ let playerWin = "You Win!"
 let playerLose = "You Lose!"
 let gameTie = "It's a Tie!"
 
-selections.forEach(button)(button => addEventListener('click', () => {
-    player = button.textContent
-    computer = getComputerChoice()
-    playerText.textContent = `You : ${player}`
-    computerText.textContent = `Computer : ${computer}`
-    resultText.textContent = playRound()
-}))
+selectionButtons.forEach(selection =>{
+    selection.addEventListener('click', e =>{
+        player = selection.textContent 
+        computer = getComputerChoice()
+        playerText.textContent = `You : ${player}`
+        computerText.textContent = `Computer : ${computer}`
+        resultText.textContent = playRound()
+    })
+})
 
 function getComputerChoice(){
     let items = ["✊","✋","✌️"]
